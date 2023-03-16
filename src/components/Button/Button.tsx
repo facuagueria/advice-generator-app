8f27a2eee88b01e989399ab41fc5fea74ad6cbcc
facuagueria <facundoagueria1@gmail.com>
2023-03-16 20:47:58 -0300

    fix position button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -8,7 +8,7 @@ type Props = {
 function Button({ handleClickButton, children }: Props): JSX.Element {
   return (
     <button
-      className="absolute bottom-64 mt-10 flex h-[64px] w-[64px] items-center justify-center rounded-[68px] bg-neon-green hover:shadow-[0_0_40px] hover:shadow-neon-green active:shadow-[0_0_40px] active:shadow-neon-green"
+      className="absolute -bottom-8 flex h-[64px] w-[64px] items-center justify-center rounded-[68px] bg-neon-green hover:shadow-[0_0_40px] hover:shadow-neon-green active:shadow-[0_0_40px] active:shadow-neon-green"
       onClick={handleClickButton}
     >
       {children}
