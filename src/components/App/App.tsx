cf6fffa1e359ff29b3c461ba39ed70492f984614
facuagueria <facundoagueria1@gmail.com>
2023-03-16 20:48:15 -0300

    Remove unused bg color from App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,7 @@
 import { Advisor } from '../index'
 function App() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-dark-blue px-2 md:px-0">
+    <div className="flex h-screen w-screen items-center justify-center px-2 md:px-0">
       <Advisor />
     </div>
   )
